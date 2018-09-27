e107d05232ee61a753d635549249817c212a54de
Chintan Shah <chintan@wohlig.com>
2018-09-28 00:51:34 +0530

    ok

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = !process.env.NODE_ENV ? "development" : process.env.NODE_ENV
-global.pwd = process.env.PWD;
+global.pwd = "../..";
 global.env = require(pwd + "/config/env/" + process.env.NODE_ENV).default;
 require("./lib/globals")
 require(pwd + "/config/globals")

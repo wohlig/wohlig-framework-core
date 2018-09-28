36dba8c9de6cb8c67f04b7d597e32bbc19be089c
Chintan Shah <chintan@wohlig.com>
2018-09-28 08:41:08 +0530

    ok

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ require(pwd + "/config/middleware.js")
 require("./lib/responses.js")
 require("./lib/controllers.js")
 require("./lib/models.js")
+require("./lib/views.js")
 app.listen(port, () => {
     console.log(`Server Started at Port ${port}`)
 })

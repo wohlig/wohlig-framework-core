e9a250a1e95009ebe2489e3c4633ad7600335d2d
Chintan Shah <chintan@wohlig.com>
2018-12-27 17:45:14 +0530

    changes for the server port

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-process.env.NODE_ENV = !process.env.NODE_ENV ? "development" : process.env.NODE_ENV
+process.env.NODE_ENV = !process.env.NODE_ENV
+  ? "development"
+  : process.env.NODE_ENV;
 global.pwd = __dirname + "/../..";
 global.env = require(pwd + "/config/env/" + process.env.NODE_ENV).default;
-require("./lib/globals")
-require(pwd + "/config/globals")
-require("./lib/mongoose.js")
-require("./lib/express.js")
-require(pwd + "/config/middleware.js")
-require("./lib/responses.js")
-require("./lib/controllers.js")
-require("./lib/models.js")
-require("./lib/views.js")
-app.listen(port, () => {
-    console.log(`Server Started at Port ${port}`)
-    require(pwd + "/config/cron");
-})
+require("./lib/globals");
+require(pwd + "/config/globals");
+require("./lib/mongoose.js");
+require("./lib/express.js");
+require(pwd + "/config/middleware.js");
+require("./lib/responses.js");
+require("./lib/controllers.js");
+require("./lib/models.js");
+require("./lib/views.js");
+app.listen(env.port, () => {
+  console.log(`Server Started at Port ${port}`);
+  require(pwd + "/config/cron");
+});

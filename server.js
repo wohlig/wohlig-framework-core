e4801abcbfddccd2f196389b29ea266728bf4502
Chintan Shah <chintan@wohlig.com>
2021-03-17 21:23:36 +0530

    changes

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ require("./lib/responses.js");
 require("./lib/controllers.js");
 require("./lib/models.js");
 require("./lib/views.js");
-require("./lib/test.js");
 
 app.listen(env["PORT"], () => {
   console.log(`Server Started at Port ${env["PORT"]}`);
